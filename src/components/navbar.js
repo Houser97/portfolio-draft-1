60316d41eca20675229556ee9a2c0a4d8fa6c4a1
Houser97 <arturo.riverar97@gmail.com>
2022-09-08 21:07:20 -0500

    Create onScroll state in navbar to dictate which class assign it

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,9 +1,28 @@
+import { useEffect, useState } from 'react';
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import NavDropdown from 'react-bootstrap/NavDropdown';
 
 function navBar() {
+
+  const [activeLink, setActiveLink] = useState('home');
+  const [scrolled, setScrolled] = useState(false);
+
+  useEffect(() => {
+    const onScroll = () => {
+      if(window.scrollY > 50) {
+        setScrolled(true);
+      } else {
+        setScrolled(false)
+      }
+    };
+
+    window.addEventListener('scroll', onScroll);
+
+    return () => window.removeEventListener('scroll', onScroll);
+  }, [])
+
   return (
     <Navbar bg="light" expand="lg">
       <Container>
@@ -12,18 +31,15 @@ function navBar() {
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
             <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#link">Link</Nav.Link>
-            <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-              <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.2">
-                Another action
-              </NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-              <NavDropdown.Divider />
-              <NavDropdown.Item href="#action/3.4">
-                Separated link
-              </NavDropdown.Item>
-            </NavDropdown>
+            <Nav.Link href="#link">Skills</Nav.Link>
+            <Nav.Link href="#link">Projects</Nav.Link>
+            <span className='navbar-text'>
+              <div className='social-icon'>
+                <a href= '#'><img alt=''></img></a>
+                <a href= '#'><img alt=''></img></a>
+                <a href= '#'><img alt=''></img></a>
+              </div>
+            </span>
           </Nav>
         </Navbar.Collapse>
       </Container>
