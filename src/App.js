49911073812848372414e662cb9a75a04d1e151a
Houser97 <arturo.riverar97@gmail.com>
2022-09-10 12:29:48 -0500

    Create Navbar component and import it to App.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
 import Navbar from './components/navbar';
+import Banner from './components/banner';
 import 'bootstrap/dist/css/bootstrap.css';
 
 function App() {
   return (
     <div className="App">
       <Navbar />
+      <Banner />
     </div>
   );
 }
