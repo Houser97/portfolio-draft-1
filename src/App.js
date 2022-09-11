98771ce82c8a187eb5db01e339d0a837d3fef1d9
Houser97 <arturo.riverar97@gmail.com>
2022-09-10 23:12:08 -0500

    Import Projects component to App.js

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Navbar from './components/navbar';
 import Banner from './components/banner';
 import 'bootstrap/dist/css/bootstrap.css';
 import Skills from './components/skills';
+import Projects from './components/projects';
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
       <Navbar />
       <Banner />
       <Skills />
+      <Projects />
     </div>
   );
 }
