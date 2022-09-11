686a9419b59a8545434dcdb5bd552e675cecc8ec
Houser97 <arturo.riverar97@gmail.com>
2022-09-11 16:59:01 -0500

    Install animate.css dependency

--- src/components/footer.js
+++ src/components/footer.js
@@ -15,9 +15,9 @@ const Footer = () => {
           </Col>
           <Col size={12} sm={6} className="text-center text-sm-end">
             <div className="social-icon">
-              <a href="#"><img src={navIcon1} alt="Icon" /></a>
-              <a href="#"><img src={navIcon2} alt="Icon" /></a>
-              <a href="#"><img src={navIcon3} alt="Icon" /></a>
+                <a className="linkedin" href="#"><img src={navIcon1} alt="" /></a>
+                <a className="facebook" href="#"><img src={navIcon2} alt="" /></a>
+                <a className="instagram" href="#"><img src={navIcon3} alt="" /></a>
             </div>
             <p>Copyright 2022. All Rights Reserved</p>
           </Col>
