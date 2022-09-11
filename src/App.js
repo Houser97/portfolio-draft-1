895f72b8fa096188324ef2091c0e1052fe12f9a7
Houser97 <arturo.riverar97@gmail.com>
2022-09-10 22:41:59 -0500

    Style Skills component and add it to App.js

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import './App.css';
 import Navbar from './components/navbar';
 import Banner from './components/banner';
 import 'bootstrap/dist/css/bootstrap.css';
+import Skills from './components/skills';
 
 function App() {
   return (
     <div className="App">
       <Navbar />
       <Banner />
+      <Skills />
     </div>
   );
 }
