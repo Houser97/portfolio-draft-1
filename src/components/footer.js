972cbe0e9c6d2b17e20ded74342839d3e56dbca3
Houser97 <arturo.riverar97@gmail.com>
2022-09-18 19:15:10 -0500

    Fix footer by removing container and adding padding

--- src/components/footer.js
+++ src/components/footer.js
@@ -8,21 +8,19 @@ import '../styles/footer.css';
 const Footer = () => {
   return (
     <footer className="footer">
-      <Container>
-        <Row className="align-items-center">
-          <Col size={12} sm={6}>
-            <img src={logo} alt="Logo" />
-          </Col>
-          <Col size={12} sm={6} className="text-center text-sm-end">
-            <div className="social-icon">
-                <a className="linkedin" href="#"><img src={navIcon1} alt="" /></a>
-                <a className="facebook" href="#"><img src={navIcon2} alt="" /></a>
-                <a className="instagram" href="#"><img src={navIcon3} alt="" /></a>
-            </div>
-            <p>Copyright 2022. All Rights Reserved</p>
-          </Col>
-        </Row>
-      </Container>
+      <div className="align-items-center footer-container">
+        <div className="footer-logo">
+          <div className="logo-architecture">IT'S ALL ABOUT </div>
+          <div className="logo-architecture">ARCHITECTURE</div>
+        </div>
+        <Col size={12} sm={6} className="text-center text-sm-end">
+          <div className="social-icon">
+              <a className="linkedin" href="#"><img src={navIcon1} alt="" /></a>
+              <a className="facebook" href="#"><img src={navIcon2} alt="" /></a>
+              <a className="instagram" href="#"><img src={navIcon3} alt="" /></a>
+          </div>
+        </Col>
+      </div>
     </footer>
   )
 }
