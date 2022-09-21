f51cce4283cc0df359e9cea481bd4d68207e6e53
Houser97 <arturo.riverar97@gmail.com>
2022-09-20 21:23:48 -0500

    Remove BrowserRouter component from Navbar

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,7 +1,6 @@
 import { useState } from "react";
 import { Navbar, Nav, Container } from "react-bootstrap";
 import navIcon3 from '../assets/img/nav-icon3.svg';
-import { BrowserRouter as Router} from "react-router-dom";
 import '../styles/navbar.css'
 
 const NavbarReact = () => {
