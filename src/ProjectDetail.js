c29952d196b8196577c445260addbc3027c9a2b9
Houser97 <arturo.riverar97@gmail.com>
2022-09-20 21:37:53 -0500

    Create ProjectDetail stylesheet and import it into component

--- src/ProjectDetail.js
+++ src/ProjectDetail.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import Navbar from './components/navbar';
+import './styles/ProjectDetail.css'
 
 const ProjectDetail = () => {
   return (
     <div className='project-detail-template'>
         <Navbar isAppComponent={false}/>
+        Houser
     </div>
   )
 }
