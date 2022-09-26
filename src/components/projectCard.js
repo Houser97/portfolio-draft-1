1985631f7ad5895d4a5645225e8e2a923c77fce1
Houser97 <arturo.riverar97@gmail.com>
2022-09-25 20:32:21 -0500

    Import Link from router to redirect users to path according to project

--- src/components/projectCard.js
+++ src/components/projectCard.js
@@ -1,14 +1,27 @@
+import { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
 import '../styles/projectCard.css'
 
 const ProjectCard = ({ title, description, imgUrl }) => {
+
+  const [formattedTitle, setFormattedTitle] = useState(title)
+
+  useEffect(() => {
+    let formatTitle = title.replace(/\s/g, '-');
+    setFormattedTitle(formatTitle);
+  }, [])
+  
+
   return (
-      <a href='project' className="proj-imgbx">
+    <Link to = {`project/${formattedTitle}`} className = 'link-router'>
+      <div href='project' className="proj-imgbx">
         <img src={imgUrl} className = 'img-project' alt="project"/>
         <div className="proj-txtx">
           <h4>{title}</h4>
           <span>{description}</span>
         </div>
-      </a>
+      </div>
+    </Link>
   )
 }
 
