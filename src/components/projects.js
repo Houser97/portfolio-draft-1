b795d48cb1fc7e899b2705b9e637e8830d089933
Houser97 <arturo.riverar97@gmail.com>
2022-09-25 20:26:18 -0500

    Remote imports of images from Projects component

--- src/components/projects.js
+++ src/components/projects.js
@@ -1,15 +1,5 @@
 import { Row, Col, Tab, Nav } from "react-bootstrap";
 import ProjectCard from "./projectCard";
-import Ext1 from "../assets/img/projects/Ext1.jpg";
-import Ext2 from "../assets/img/projects/Ext2.jpg";
-import Int2 from "../assets/img/projects/Int2.jpg";
-import INTERIORCERAMICA from "../assets/img/projects/INTERIORCERAMICAYMETAL1-4-2.jpg";
-import InteriorPlasticoyVidrio from "../assets/img/projects/InteriorPlasticoyVidrio.jpg";
-import IntRen from "../assets/img/projects/IntRen.jpg";
-import Livingroom1 from "../assets/img/projects/Livingroom1.jpg";
-import Romantico2 from "../assets/img/projects/Romantico2-2.jpg";
-import bedroom from "../assets/img/projects/bedroom.jpg";
-import babyBedroom from "../assets/img/projects/babyBedroom.jpg";
 import TrackVisibility from 'react-on-screen';
 import '../styles/projects.css';
 import { projects } from "../constants/constants";
