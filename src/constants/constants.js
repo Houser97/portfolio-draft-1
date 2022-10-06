ce507f5aafb9a8f2de4bd71c40b996a0bdc00e80
Houser97 <arturo.riverar97@gmail.com>
2022-10-05 21:53:17 -0500

    Import LivingRoom images into 'constants' file

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -1,17 +1,14 @@
-import Ext1 from "../assets/img/projects/Ext1.jpg";
-import Ext2 from "../assets/img/projects/Ext2.jpg";
-import Int2 from "../assets/img/projects/Int2.jpg";
-import INTERIORCERAMICA from "../assets/img/projects/INTERIORCERAMICAYMETAL1-4-2.jpg";
-import InteriorPlasticoyVidrio from "../assets/img/projects/InteriorPlasticoyVidrio.jpg";
-import IntRen from "../assets/img/projects/IntRen.jpg";
-import Livingroom1 from "../assets/img/projects/Livingroom1.jpg";
-import Romantico2 from "../assets/img/projects/Romantico2-2.jpg";
-import bedroom from "../assets/img/projects/bedroom.jpg";
-import babyBedroom from "../assets/img/projects/babyBedroom.jpg";
+// Living Room
+import IndustrialLiving from '../assets/img/projects/LivingRoom/IndustrialLiving.jpg';
+import Eclectic from '../assets/img/projects/LivingRoom/EclecticLiving.jpg';
+import GlamLiving from '../assets/img/projects/LivingRoom/GlamLiving.jpg';
+import Scandi from '../assets/img/projects/LivingRoom/ScandiLiving.jpg';
+import FarmHouse from '../assets/img/projects/LivingRoom/FarmhouseLiving.jpg';
+import Romantic from '../assets/img/projects/LivingRoom/RomanticLiving.jpg';
 
 export const projects = [
     {
-      title: "Ext1",
+      title: "",
       description: "Design & Development AHASA AS ASA ASAS ASSDF",
       imgUrl: Ext1,
     },
