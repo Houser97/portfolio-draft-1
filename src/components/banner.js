0399855373b8ba898a75a999130473b637c493cc
Houser97 <arturo.riverar97@gmail.com>
2022-10-07 22:31:37 -0500

    Add class to banner description and justify text

--- src/components/banner.js
+++ src/components/banner.js
@@ -49,7 +49,7 @@ const Banner = () => {
                 <span className="tagline">Hi! I'm Andrea Vidal</span>
                 {/*Acá va la parte que se escribe dinámicamente */}
                 <h1><span className="txt-rotate"><span className="wrap">{text}</span></span></h1>
-                    <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
+                    <p className="description-banner">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
                     {/*<button onClick={() => console.log('connect')}>Let’s Connect <ArrowRightCircle size={25} /></button>*/}
             </div>
                
